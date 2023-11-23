a2db45f7a2d99f071382fd99ae08fbd89c57339d
lucianoc <lucianoc@student.21-school.ru>
2023-11-24 00:31:32 +0300

    Edit README and replace ilya with ~

--- Map_Generator/lirs-wct/lirs_wct_console/build/cmake_install.cmake
+++ Map_Generator/lirs-wct/lirs_wct_console/build/cmake_install.cmake
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/ilya/lirs-wct/lirs_wct_console/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "~/lirs-wct/lirs_wct_console/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
